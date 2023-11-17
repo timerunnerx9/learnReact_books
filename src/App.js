02337385300d9b2a2273fde2efe1739fccec22e6
dave cen <davecen9@gmail.com>
2023-11-17 16:32:58 -0500

    test

--- src/App.js
+++ src/App.js
@@ -36,6 +36,7 @@ function App() {
 
   return (
     <div className="app">
+      <h1>Reading List</h1>
       <BookCreate onCreate={createBook} />
       <BookList onDelete={deleteBookById} books={books} onEdit={editBookById}/>
     </div>
