985429dc69ffd2b81fd9d0a0997a387cbcebd621
dave cen <davecen9@gmail.com>
2023-11-16 19:29:45 -0500

    save

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import {useState} from "react";
+import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+import BookShow from "./components/BookShow";
 
-function App (){
-    const[books, setBooks] = useState([]);
+function App() {
+  const [books, setBooks] = useState([]);
 
-    const createBook = (title) =>{
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
 
-        console.log("Need to add book with:", title);
+    setBooks(updatedBooks);
+  };
 
-    };
+  const createBook = (title) => {
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 1000), title: title },
+    ];
 
+    setBooks(updatedBooks);
+  };
 
-    
-    return <div><BookCreate onCreate={createBook}/></div>;
+  return (
+    <div className="app">
+      <BookCreate onCreate={createBook} />
+      <BookList books={books} />
+    </div>
+  );
 }
 
 export default App;
