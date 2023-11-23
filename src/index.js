80fdb93f11eef3ebd004014d4f85853b2e152906
dave cen <davecen9@gmail.com>
2023-11-23 11:00:04 -0500

    save

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import {Provider} from "./context/books";
 
 const el = document.getElementById("root");
 const root = ReactDom.createRoot(el);
 
-root.render(<App/>)
+root.render(
+  <Provider>
+    <App />
+  </Provider>
+);
